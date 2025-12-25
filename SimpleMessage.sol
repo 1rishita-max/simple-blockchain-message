cdb77d79beb5c4142c5939c48e9f5f0b01258456
1rishita-max <rishitapathak73@gmail.com>
2025-12-25 18:40:51 +0530

    Create SimpleMessage contract with message storage

---
+++ SimpleMessage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleMessage {
+
+    string public message;
+
+    function setMessage(string memory _msg) public {
+        message = _msg;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
